629b6ae93107ebe7385e722f483fa8c539c1e864
krissidibe <assowlove@gmail.com>
2023-12-27 07:54:39 +0000

    27 Dec bugs611

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -257,7 +257,6 @@ if(dataUser?.error?.message.includes("authentification")){
                     setShowNotif((x) => (x = false));
                     setSearch("");
                     modal.onOpen();
-                    alert(modal)
                     setShowSearchInput((x) => (x = false));
                     setSearchProject("")
 setStatutSort("");
